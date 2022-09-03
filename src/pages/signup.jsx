49281c0a07ced7caa34a82a581a66cfd640000bb
Sitanshu Pokalwar <sitz2309@gmail.com>
2022-09-03 13:03:58 +0530

    using router old version

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -2,40 +2,9 @@ import React from "react";
 import lottie from "lottie-web";
 import Doctor from "../assets/images/doclottie.json";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-//import { Link } from "react-router-dom";
 import SignUpContent from "../components/SignUpContent";
 import SignInContent from "../components/SignInContent";
 
-// export default function Signup() {
-//     React.useEffect(() => {
-//         lottie.loadAnimation({
-//             container: document.querySelector("#docLottie"),
-//             animationData: Doctor,
-//             loop: true, // boolean
-//             autoplay: true, // boolean
-//         });
-//     }, []);
-
-//     return (
-//         <section className='box-border grid place-content-center min-h-screen min-w-full bg-[#FEF8ED]'>
-//             <div className='container flex w-screen'>
-//                 <Link to='/'>
-//                     <SignUpContent />
-//                 </Link>
-//                 <Link to='/signin'>
-//                     <SignInContent />
-//                 </Link>
-
-//                 <div className='basis-[50%] overflow-x-visible'>
-//                     <div
-//                         id='docLottie'
-//                         style={{ width: 700, height: 700 }}
-//                     ></div>
-//                 </div>
-//             </div>
-//         </section>
-//     );
-// }
 
 export default function Signup() {
     React.useEffect(() => {
